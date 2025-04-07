94eee45edb25951c593763a34c3cff4ad172d63b
gwon tae-yun <taeyun1118@naver.com>
2025-04-07 16:41:11 +0900

    init

---
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/MomentreeblogApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.momentreeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentreeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentreeblogApplication.class, args);
+	}
+
+}
---
+++ backend/momentreeblog/src/test/java/com/likelion/momentreeblog/MomentreeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.momentreeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentreeblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
