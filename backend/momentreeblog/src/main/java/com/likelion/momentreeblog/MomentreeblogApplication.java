36f7332d580abd4563966338f63a6f176be7eb8d
gwon tae-yun <taeyun1118@naver.com>
2025-04-08 09:33:20 +0900

    edit application.yml

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/MomentreeblogApplication.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/MomentreeblogApplication.java
@@ -1,5 +1,6 @@
 package com.likelion.momentreeblog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
