c0dd73b889497d7610bfc46ab7845141e19a3a2e
gwon tae-yun <taeyun1118@naver.com>
2025-04-09 20:31:51 +0900

    feat: 패키지 추가, BaseEntity 추가

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/MomentreeblogApplication.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/MomentreeblogApplication.java
@@ -3,7 +3,9 @@ package com.likelion.momentreeblog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MomentreeblogApplication {
 
