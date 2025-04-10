dd83caabdae53b44b44faba76599e58564a687f4
bang <quddn2917@gmail.com>
2025-04-10 10:28:00 +0900

    feat: comment,role 엔티티 추가(#16)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/comment/entity/Comment.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/comment/entity/Comment.java
@@ -1,4 +1,33 @@
 package com.likelion.momentreeblog.domain.board.comment.entity;
 
-public class Comment {
+import com.likelion.momentreeblog.domain.board.board.entity.Board;
+import com.likelion.momentreeblog.domain.user.user.entity.User;
+import com.likelion.momentreeblog.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
 }
