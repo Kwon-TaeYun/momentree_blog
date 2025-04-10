6e4f9c6b9768d5367110f26ee89095feff705f7d
bang <quddn2917@gmail.com>
2025-04-10 13:38:58 +0900

    feat: ãpr을 (#16)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/comment/entity/Comment.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/comment/entity/Comment.java
@@ -1,4 +1,31 @@
 package com.likelion.momentreeblog.domain.board.comment.entity;
 
-public class Comment {
+import com.likelion.momentreeblog.domain.board.board.entity.Board;
+import com.likelion.momentreeblog.domain.user.user.entity.User;
+import com.likelion.momentreeblog.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "comments")
+@ToString
+public class Comment extends BaseEntity {
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
 }
--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/role/entity/Role.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/role/entity/Role.java
@@ -1,4 +1,23 @@
 package com.likelion.momentreeblog.domain.user.role.entity;
 
-public class Role {
+
+import com.likelion.momentreeblog.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+@SuperBuilder
+@ToString
+public class Role extends BaseEntity {
+
+    @Column( nullable = false)
+    private String name;
 }
