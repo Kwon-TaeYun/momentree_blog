5baed6a2e862d3f3d75d8d1e2ce52ab466736fdc
vincentius <parkok6143@gmail.com>
2025-04-10 10:43:24 +0900

    feat:Category,Like 추가(#14)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/like/entity/Like.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/like/entity/Like.java
@@ -1,4 +1,30 @@
 package com.likelion.momentreeblog.domain.board.like.entity;
 
-public class Like {
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "like")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@ToString
+public class Like extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
