995592086538601597feaf6f528a0b9495fa59df
Morgan <morgan109jb@gmail.com>
2025-04-10 15:57:24 +0900

    feat: Blog, Follower 엔티티 추가 (#17)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/follower/entity/Follower.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/follower/entity/Follower.java
@@ -1,4 +1,32 @@
 package com.likelion.momentreeblog.domain.user.follower.entity;
 
+import com.likelion.momentreeblog.domain.user.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Follower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 팔로우한 사람
+    @ManyToOne
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    // 팔로우 당한 사람
+    @ManyToOne
+    @JoinColumn(name = "following_id", nullable = false)
+    private User following;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
