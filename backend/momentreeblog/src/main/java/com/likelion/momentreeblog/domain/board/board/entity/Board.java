cd5e02d1470b1bec1a696334d80b8ccdd2b94f12
pickipi <sewd1224@gmail.com>
2025-04-10 10:45:30 +0900

    feat: Board 엔티티 추가

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/board/entity/Board.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/board/entity/Board.java
@@ -1,7 +1,10 @@
 package com.likelion.momentreeblog.domain.board.board.entity;
 
+import com.likelion.momentreeblog.domain.blog.blog.entity.Blog;
+import com.likelion.momentreeblog.domain.board.category.entity.Category;
+import com.likelion.momentreeblog.domain.board.like.entity.Like;
 import com.likelion.momentreeblog.global.jpa.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,26 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString
 public class Board extends BaseEntity {
-    private String title;
-    private String content;
+    @Column(nullable = false)
+    private String title; // 제목
+
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String content; // 내용
+
+    @Column(name = "main_photo_id", nullable = false)
+    private Long mainPhotoId; // 대표사진ID
+
+    @Column(name = "photo_saved_url")
+    private String photoSavedUrl; // 사진저장주소
+
+    @OneToOne
+    @JoinColumn(name = "blog_id")
+    private Blog blog;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+    private Like like;
+
+
 }
