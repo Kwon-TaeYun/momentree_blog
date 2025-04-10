37de651860b315c63c8796411cb625d396beb46d
bang <quddn2917@gmail.com>
2025-04-10 10:28:00 +0900

    feat: comment,role 엔티티 추가(#16)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/role/entity/Role.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/role/entity/Role.java
@@ -1,4 +1,23 @@
 package com.likelion.momentreeblog.domain.user.role.entity;
 
-public class Role {
+
+import com.likelion.momentreeblog.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role extends BaseEntity {
+
+    @Column(name = "role_name", nullable = false)
+    private String role;
 }
