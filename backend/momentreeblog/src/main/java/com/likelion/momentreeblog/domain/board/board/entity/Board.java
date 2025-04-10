4f7baaf71d01654be73237daa138a498ed2753e3
pickipi <sewd1224@gmail.com>
2025-04-10 13:25:58 +0900

    feat: 브랜치 이동 후 Board 엔티티 추가 (#13)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/board/entity/Board.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/board/board/entity/Board.java
@@ -1,7 +1,10 @@
 package com.likelion.momentreeblog.domain.board.board.entity;
 
+import com.likelion.momentreeblog.domain.blog.blog.entity.Blog;
+import com.likelion.momentreeblog.domain.board.category.entity.Category;
+import com.likelion.momentreeblog.domain.board.like.entity.Like;
 import com.likelion.momentreeblog.global.jpa.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +17,28 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @ToString
+@Table(name = "board")
 public class Board extends BaseEntity {
-    private String title;
-    private String content;
+    @Column(nullable = false)
+    private String title; // 제목
+
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String content; // 내용
+
+    @Column(name = "main_photo_id", nullable = false)
+    private Long mainPhotoId; // 대표사진ID
+
+    @Column(name = "photo_saved_url")
+    private String photoSavedUrl; // 사진저장주소
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id", nullable = false)
+    private Blog blog; // 블로그 테이블
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Like like; // 좋아요 테이블
 }
