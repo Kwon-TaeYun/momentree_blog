83927e1052403c6d49a8995052bb621ff34dadc8
gwon tae-yun <taeyun1118@naver.com>
2025-04-11 14:07:17 +0900

    fix: db_dev gitignore 추가(#32)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/MomentreeblogApplication.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/MomentreeblogApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class MomentreeblogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MomentreeblogApplication.class, args);
 	}
