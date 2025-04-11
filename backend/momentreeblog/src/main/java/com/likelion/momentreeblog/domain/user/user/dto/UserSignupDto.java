3fc5208e9a88c68243a3b3665e99a512875ce5e5
gwon tae-yun <taeyun1118@naver.com>
2025-04-11 11:44:34 +0900

    feat: 회원가입 시 비밀번호 입력 조건 추가(#32)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/dto/UserSignupDto.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/dto/UserSignupDto.java
@@ -1,6 +1,7 @@
 package com.likelion.momentreeblog.domain.user.user.dto;
 
 import com.likelion.momentreeblog.domain.blog.blog.entity.Blog;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +13,10 @@ import lombok.NoArgsConstructor;
 public class UserSignupDto {
     private String name;
     private String email;
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{10,}$|^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자 중 2종류 이상 포함 10자 이상 또는 3종류 이상 포함 8자 이상이어야 합니다."
+    )
     private String password;
     private String blogName;
     private String refreshToken;
