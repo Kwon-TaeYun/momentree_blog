21ad50cf330de50df02ca6f3723366af861ab4d7
pickipi <sewd1224@gmail.com>
2025-04-11 16:13:25 +0900

    feat: Follow 관련 Service 작성 (#35)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/service/FollowService.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/service/FollowService.java
@@ -1,5 +1,10 @@
 package com.likelion.momentreeblog.domain.user.user.service;
 
+import com.likelion.momentreeblog.domain.user.user.entity.User;
+
 // 팔로우/언팔로우, 팔로우 수 확인
-public class FollowService {
+public interface FollowService {
+    void follow(User follower, User following);
+    void unfollow(User follower, User following);
+    boolean isFollowing(User follower, User following);
 }
