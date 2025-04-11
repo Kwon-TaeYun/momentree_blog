7600fe3312765afbd2456aaeca5b819486e916a6
pickipi <sewd1224@gmail.com>
2025-04-11 15:49:20 +0900

    feat: user dto 생성 및 UserFind Controller 작성 (#35)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/controller/UserFindApiV1Controller.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/controller/UserFindApiV1Controller.java
@@ -1,5 +1,38 @@
 package com.likelion.momentreeblog.domain.user.user.controller;
 
 // 중복 검사, 유저 조회 전용
+
+import com.likelion.momentreeblog.domain.user.user.dto.UserResponse;
+import com.likelion.momentreeblog.domain.user.user.entity.User;
+import com.likelion.momentreeblog.domain.user.user.service.UserFindService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
 public class UserFindApiV1Controller {
+    private final UserFindService userFindService;
+
+    @GetMapping("/id")
+    public ResponseEntity<UserResponse> getUserById(@RequestParam String id){
+        User user = userFindService.getUserByEmail(id);
+        return ResponseEntity.ok(UserResponse.from(user));
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<UserResponse> getUserByName(@RequestParam String name){
+        User user = userFindService.getUserByEmail(name);
+        return ResponseEntity.ok(UserResponse.from(user));
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<UserResponse> getUserByEmail(@RequestParam String email){
+        User user = userFindService.getUserByEmail(email);
+        return ResponseEntity.ok(UserResponse.from(user));
+    }
 }
---
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/dto/UserResponse.java
@@ -0,0 +1,21 @@
+package com.likelion.momentreeblog.domain.user.user.dto;
+
+import com.likelion.momentreeblog.domain.user.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserResponse {
+    private Long id; // 아이디
+    private String name; // 이름
+    private String email; // 이메일
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+}
