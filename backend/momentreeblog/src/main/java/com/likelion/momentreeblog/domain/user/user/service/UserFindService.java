0bd1d2d1a6f8f72bf8b5a203ef4bb97f505cf475
pickipi <sewd1224@gmail.com>
2025-04-11 10:39:51 +0900

    feat: UserFindService 분리 및 메소드 구체화 (#35)

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/service/UserFindService.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/user/user/service/UserFindService.java
@@ -1,5 +1,12 @@
 package com.likelion.momentreeblog.domain.user.user.service;
 
-// 이메일/닉네임 중복 체크, 아이디 찾기
-public class UserFindService {
+import com.likelion.momentreeblog.domain.user.user.entity.User;
+
+// 이메일/닉네임 중복 체크, 아이디 찾기 (정의)
+public interface UserFindService {
+    User getUserByEmail(String email);
+    User getUserByName(String name);
+
+    Long getFollowingCount(User user);
+    Long getFollowerCount(User user);
 }
