632387cb9796fc876c930ff0172077c28adfda62
taeyun <taeyun1118@naver.com>
2025-04-13 14:18:22 +0900

    Delete backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/blog/blog/repository/BlogRepository.java

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/blog/blog/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.likelion.momentreeblog.domain.blog.blog.repository;
-
-import com.likelion.momentreeblog.domain.blog.blog.entity.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-
-}
