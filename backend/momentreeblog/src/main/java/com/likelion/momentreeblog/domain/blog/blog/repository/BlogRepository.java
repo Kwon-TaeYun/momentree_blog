201e3208c337192d1c7e4475275278b4a3ea9437
taeyun <taeyun1118@naver.com>
2025-04-23 09:22:42 +0900

    Update BlogRepository.java

--- backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/blog/blog/repository/BlogRepository.java
+++ backend/momentreeblog/src/main/java/com/likelion/momentreeblog/domain/blog/blog/repository/BlogRepository.java
@@ -5,8 +5,6 @@ import com.likelion.momentreeblog.domain.user.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.awt.print.Pageable;
-import java.util.List;
 import java.util.Optional;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
