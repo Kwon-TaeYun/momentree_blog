041738ccc8522a67d5635b8cec0ea503c6f0478a
gwon tae-yun <taeyun1118@naver.com>
2025-04-29 10:05:40 +0900

    package.json 버전 수정

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,15 +1,22 @@
-'use client';
-import { LoginMemberContext, useLoginMember } from '@/stores/auth/loginMember'
-import Link from 'next/link';
-import Image from 'next/image';
-import { useEffect } from 'react'
-import { BiSearch } from 'react-icons/bi'; // BiSearch 아이콘도 필요함
-import React from 'react';
-import Footer from '@/components/layout/Footer';
-import Header from '@/components/layout/Header';
+"use client";
+import { LoginMemberContext, useLoginMember } from "@/stores/auth/loginMember";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect } from "react";
+import { BiSearch } from "react-icons/bi"; // BiSearch 아이콘도 필요함
+import React from "react";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 export function ClientLayout({ children }: { children: React.ReactNode }) {
-  const { loginMember, setLoginMember, setNoLoginMember, isLoginMemberPending, isLogin, logout, logoutAndHome } =
-      useLoginMember()
+  const {
+    loginMember,
+    setLoginMember,
+    setNoLoginMember,
+    isLoginMemberPending,
+    isLogin,
+    logout,
+    logoutAndHome,
+  } = useLoginMember();
 
   // 전역관리를 위한 Store 등록 - context api 사용
   const loginMemberContextValue = {
@@ -19,27 +26,27 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
     isLogin,
     logout,
     logoutAndHome,
-  }
+  };
   useEffect(() => {
-    fetch('http://localhost:8090/api/v1/members/me', {
-      credentials: 'include',
+    fetch("http://localhost:8090/api/v1/members/me", {
+      credentials: "include",
     })
-        .then((response) => response.json())
-        .then((data) => {
-          setLoginMember(data)
-        })
-        .catch((error) => {
-          setNoLoginMember()
-        })
-  }, [])
+      .then((response) => response.json())
+      .then((data) => {
+        setLoginMember(data);
+      })
+      .catch((error) => {
+        setNoLoginMember();
+      });
+  }, []);
 
   return (
-      <LoginMemberContext value={loginMemberContextValue}>
-        <main>
-          <Header/>
-          {children}
-          <Footer/>
-        </main>
-      </LoginMemberContext>
+    <LoginMemberContext.Provider value={loginMemberContextValue}>
+      <main>
+        <Header />
+        {children}
+        <Footer />
+      </main>
+    </LoginMemberContext.Provider>
   );
 }
