800ad4efddcf2111d50ef8a81facc1d232293418
gwon tae-yun <taeyun1118@naver.com>
2025-04-29 12:12:22 +0900

    .

--- frontend/src/app/members/edit/[id]/page.tsx
+++ frontend/src/app/members/edit/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import Sidebar from "../../../../../components/Sidebar";
+import Sidebar from "@/components/Sidebar";
 
 export default function MemberEditPage() {
   const router = useRouter();
