9731832839f51941ff6b3f69955d57faa743ca97
gwon tae-yun <taeyun1118@naver.com>
2025-04-29 09:50:00 +0900

    프론트 vercel 성공위해

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // 빌드 중에 ESLint 검사를 건너뜁니다
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       "images.unsplash.com",
       "picsum.photos",
       "momentrees3bucket.s3.ap-northeast-2.amazonaws.com",
     ],
-    eslint: {
-      // 빌드 중에 ESLint 검사를 건너뜁니다
-      ignoreDuringBuilds: true,
-    },
   },
 };
 
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ["images.unsplash.com"],
-  },
-  eslint: {
-    // 빌드 중에 ESLint 검사를 건너뜁니다
-    ignoreDuringBuilds: true,
   },
 };
 
